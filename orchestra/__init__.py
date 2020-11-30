d9ad101cff97aeffb82a440344980473eea82472
Filippo Cremonese <filippocremonese@rev.ng>
2020-11-30 14:24:59 +0100

    Refactored cmdline options
    Split on multiple lines the help to respect maximum line length.
    Sorted options by importance/likelihood to be used by a user.

--- orchestra/__init__.py
+++ orchestra/__init__.py
@@ -1,26 +1,33 @@
 import argparse
 import sys
+
 from loguru import logger
 from tqdm import tqdm
 
 from orchestra.cmds import install_subcommands
-from orchestra.model.configuration import Configuration
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--pretend", "-n", action="store_true",
+                    help="Do not execute actions, only print what would be done")
+parser.add_argument("--quiet", "-q", action="store_true",
+                    help="Do not show output of executed commands")
+parser.add_argument("--fallback-build", "-b", action="store_true",
+                    help="Build if binary archives are not available")
+parser.add_argument("--from-source", "-B", action="store_true",
+                    help="Build all components from source")
+parser.add_argument("--no-config-cache", action="store_true",
+                    help="Do not cache generated yaml configuration")
 parser.add_argument("--loglevel", "-v", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
-parser.add_argument("--pretend", "-n", action="store_true", help="Do not execute actions, only print what would be done")
-parser.add_argument("--quiet", "-q", action="store_true", help="Do not show output of executed commands")
-parser.add_argument("--no-config-cache", action="store_true", help="Do not cache generated yaml configuration")
-parser.add_argument("--from-source", "-B", action="store_true", help="Build all components from source")
-parser.add_argument("--fallback-build", "-b", action="store_true", help="Build if binary archives are not available")
 
 subparsers = install_subcommands(parser)
+
 
 class TqdmWrapper:
     def write(self, message):
         tqdm.write(message.strip())
         sys.stdout.flush()
         sys.stderr.flush()
+
 
 def main():
     args = parser.parse_args()
